21f0d8e5b59f1420f6647ca78f66e34218ce32e0
ShadowLarkens <ShadowLarkens@gmail.com>
2025-03-29 01:23:04 -0700

    Update tgui.html and remove old IE support

--- tgui/packages/tgui-panel/chat/renderer.jsx
+++ tgui/packages/tgui-panel/chat/renderer.jsx
@@ -443,10 +443,7 @@ class ChatRenderer {
       message.node = node;
       // Query all possible selectors to find out the message type
       if (!message.type) {
-        // IE8: Does not support querySelector on elements that
-        // are not yet in the document.
-        // prettier-ignore
-        const typeDef = !Byond.IS_LTE_IE8 && MESSAGE_TYPES
+        const typeDef = MESSAGE_TYPES
           .find(typeDef => (
             typeDef.selector && node.querySelector(typeDef.selector)
           ));
@@ -549,10 +546,6 @@ class ChatRenderer {
   }
 
   saveToDisk() {
-    // Allow only on IE11
-    if (Byond.IS_LTE_IE10) {
-      return;
-    }
     // Compile currently loaded stylesheets as CSS text
     let cssText = '';
     const styleSheets = document.styleSheets;
