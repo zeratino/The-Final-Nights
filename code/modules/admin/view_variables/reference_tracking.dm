BeeStation/BeeStation-Hornet

Optimizes \ref for datums, saving qdel init time (#9492)
* Optimizes \ref for datums, saving qdel init time

* Minor optimization

* Fix refs

* Fix REF() caching

code/modules/admin/view_variables/reference_tracking.dm
@@ -117,11 +117,11 @@
 					continue //End early, don't want these logging
 				#endif
 				. = TRUE
-				log_reftracker("Found [type] \ref[src] in [datum_container.type]'s \ref[datum_container] [varname] var. [container_name]")
+				log_reftracker("Found [type] [FAST_REF(src)] in [datum_container.type]'s [FAST_REF(datum_container)] [varname] var. [container_name]")
 				continue
 
 			if(islist(variable))
-				if(DoSearchVar(variable, "[container_name] \ref[datum_container] -> [varname] (list)", recursive_limit - 1, search_time))
+				if(DoSearchVar(variable, "[container_name] [FAST_REF(datum_container)] -> [varname] (list)", recursive_limit - 1, search_time))
 					. = TRUE
 
 	else if(islist(potential_container))
@@ -139,7 +139,7 @@
 					continue //End early, don't want these logging
 				#endif
 				. = TRUE
-				log_reftracker("Found [type] \ref[src] in list [container_name].")
+				log_reftracker("Found [type] [FAST_REF(src)] in list [container_name].")
 				continue
 
 			var/assoc_val = null
@@ -152,7 +152,7 @@
 					found_refs[potential_cache] = TRUE
 					continue //End early, don't want these logging
 				#endif
-				log_reftracker("Found [type] \ref[src] in list [container_name]\[[element_in_list]\]")
+				log_reftracker("Found [type] [FAST_REF(src)] in list [container_name]\[[element_in_list]\]")
 				. = TRUE
 				continue
 			//We need to run both of these checks, since our object could be hiding in either of them
@@ -170,7 +170,7 @@
 	thing_to_del.qdel_and_find_ref_if_fail(force)
 
 /datum/proc/qdel_and_find_ref_if_fail(force = FALSE)
-	SSgarbage.reference_find_on_fail["\ref[src]"] = TRUE
+	SSgarbage.reference_find_on_fail[FAST_REF(src)] = TRUE
 	qdel(src, force)
 
 #endif

zeratino/The-Final-Nights

code/modules/admin/view_variables/reference_tracking.dm
@@ -197,19 +197,19 @@ GLOBAL_LIST_EMPTY(deletion_failures)
 			var/variable = vars_list[varname]
 
 			if(variable == src)
-				testing("Found [type] \ref[src] in [datum_container.type]'s [varname] var. [container_name]")
+				testing("Found [type] [FAST_REF(src)] in [datum_container.type]'s [FAST_REF(datum_container)] [varname] var. [container_name]")
 
 			else if(islist(variable))
-				DoSearchVar(variable, "[container_name] -> list", recursive_limit - 1)
+				DoSearchVar(variable, "[container_name] [FAST_REF(datum_container)] -> [varname] (list)", recursive_limit - 1)
 
 	else if(islist(potential_container))
 		var/normal = IS_NORMAL_LIST(potential_container)
 		for(var/element_in_list in potential_container)
 			if(element_in_list == src)
-				testing("Found [type] \ref[src] in list [container_name].")
+				testing("Found [type] [FAST_REF(src)] in list [container_name].")
 
 			else if(element_in_list && !isnum(element_in_list) && normal && potential_container[element_in_list] == src)
-				testing("Found [type] \ref[src] in list [container_name]\[[element_in_list]\]")
+				testing("Found [type] [FAST_REF(src)] in list [container_name]\[[element_in_list]\]")
 
 			else if(islist(element_in_list))
 				DoSearchVar(element_in_list, "[container_name] -> list", recursive_limit - 1)
@@ -220,7 +220,10 @@ GLOBAL_LIST_EMPTY(deletion_failures)
 
 
 /proc/qdel_and_find_ref_if_fail(datum/thing_to_del, force = FALSE)
-	SSgarbage.reference_find_on_fail[REF(thing_to_del)] = TRUE
-	qdel(thing_to_del, force)
+	thing_to_del.qdel_and_find_ref_if_fail(force)
+
+/datum/proc/qdel_and_find_ref_if_fail(force = FALSE)
+	SSgarbage.reference_find_on_fail[FAST_REF(src)] = TRUE
+	qdel(src, force)
 
 #endif