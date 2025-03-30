21139ba060cac06ad8d96b4770f51ba24ed13dc1
Comrade Niobe <126028983+ComradeNiobe@users.noreply.github.com>
2025-03-30 18:21:38 -0400

    deps take 2

--- dependencies.sh
+++ dependencies.sh
@@ -11,6 +11,8 @@ export BYOND_MINOR=1659
 export RUST_G_VERSION=3.8.0
 
 #node version
+export NODE_VERSION=22
+export NODE_VERSION_PRECISE=22.14.0
 export NODE_VERSION_LTS=22.14.0
 
 # SpacemanDMM git tag
